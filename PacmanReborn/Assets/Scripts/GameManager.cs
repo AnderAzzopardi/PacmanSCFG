b3740643e15b26ecf1d7d5599e65b1d3717320f9
AnderAzzopardi <56016944+AnderAzzopardi@users.noreply.github.com>
2022-01-24 15:47:21 +0100

    3D -> 2D
    Converted Projected from 3d to 2d cos of some complications

---
+++ PacmanReborn/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    private Camera _cam;
+    private IAstarAI[] _ais;
+	
+	[SerializeField] private GameObject _tilePrefab;
+	
+	private GridManager _GridManager;
+	public void Start()
+	{
+		//Setting the _cam to main camera
+		_cam = Camera.main;
+		//Loading the path finding
+		_ais = FindObjectsOfType<MonoBehaviour>().OfType<IAstarAI>().ToArray();
+		//Creating a new grid
+		_GridManager = new GridManager(30, 30, _tilePrefab);
+		//Updating the scan
+		AstarPath.active.Scan();
+	}
+
+
+
+	private void UpdatePathfinding()
+    {
+		for (int i = 0; i < _ais.Length; i++)
+		{
+			if (_ais[i] != null) _ais[i].SearchPath();
+		}
+	}
+
+}
