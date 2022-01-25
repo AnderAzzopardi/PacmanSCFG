e4faec0f1da9877929cfa7fb46181b2427bbcdc3
AnderAzzopardi <56016944+AnderAzzopardi@users.noreply.github.com>
2022-01-25 05:12:00 +0100

    HighScore
    Highscore is set and will save when game is exited

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -20,6 +20,7 @@ public class ScoreManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        highscore = PlayerPrefs.GetInt("highscore", 0);
         scoreText.text = score.ToString()+ " Points";
         highscoreText.text = " Highscore " + highscore.ToString();
     }
@@ -28,6 +29,10 @@ public class ScoreManager : MonoBehaviour
 
        score += 1;
        scoreText.text = score.ToString()+ " Points";
+       if(highscore < score){
+        PlayerPrefs.SetInt("highscore", score);
+       }
+       
 
     }
 
